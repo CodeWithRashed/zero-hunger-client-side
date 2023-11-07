d8f3894382347d69f289ecdf4f3d5b07a634d79b
Md Rashed Ali <tools.rashed@gmail.com>
2023-11-07 19:37:44 +0600

    status update working

--- src/Pages/ManageSingleRequest.jsx
+++ src/Pages/ManageSingleRequest.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-
+import { BiSolidDonateHeart, BiSolidUserCircle } from "react-icons/bi";
+import { Button, Modal } from "flowbite-react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { useContext, useEffect } from "react";
 
 import { toast } from "react-toastify";
@@ -9,10 +11,10 @@ import { Delivered, Pending } from "../Components/Status/Status";
 
 const ManageSingleRequest = () => {
   const { activeUser } = useContext(GlobalDataContext);
+  const [openModal, setOpenModal] = useState(false);
   const [requestedFood, setRequestedFood] = useState(null);
   const userEmail = activeUser?.email;
   const requestData = useLoaderData();
-  const [donationCount, setDonationCount] = useState([]);
 
   const currentDate = new Date();
 
@@ -23,67 +25,87 @@ const ManageSingleRequest = () => {
   });
 
   useEffect(() => {
-    console.log(requestData);
-    fetch(
-      `${import.meta.env.VITE_BACKEND_API}/api/v1/user/get/food/${
-        requestData?.foodId
-      }`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setRequestedFood(data);
-      });
+    if (requestData?.foodId) {
+      fetch(
+        `${import.meta.env.VITE_BACKEND_API}/api/v1/user/get/food/${
+          requestData?.foodId
+        }`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setRequestedFood(data);
+        });
+    }
   }, [requestData]);
 
-  //   const handleFoodRequest = async () => {
-  //     if (foodData?.donarEmail == activeUser?.email) {
-  //       toast.error("You Can Not Request Your Own Food", {
-  //         position: "top-center",
-  //         autoClose: 2000,
-  //         hideProgressBar: true,
-  //         closeOnClick: true,
-  //         pauseOnHover: false,
-  //         draggable: true,
-  //         progress: undefined,
-  //         theme: "light",
-  //       });
-  //       // return
-  //     }
+  const updateFoodStatus = async () => {
+    const updatedData = {
+      deliveryStatus: "Delivered",
+    };
+    try {
+      const requestStatusResponse = await fetch(
+        `${import.meta.env.VITE_BACKEND_API}/api/v1/user/update/food/${requestData?.foodId}`,
+        {
+          method: "PATCH",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(updatedData),
+        }
+      );
+      if (requestStatusResponse.ok) {
+        await requestStatusResponse.json();
+  
+        toast.success("Accepted!", {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-  // const requestFoodData = {
-  //   foodId: foodData._id,
-  //   foodImage: foodData.foodImage,
-  //   donarImage: foodData.donarImage,
-  //   requesterName: activeUser.displayName,
-  //   requesterImage: activeUser.photoURL,
-  //   donarName: foodData.donarName,
-  //   requesterEmail: activeUser.email,
-  //   donarEmail: foodData.donarEmail,
-  //   requestDate: new Date().toISOString().split("T")[0],
-  //   requestTime: new Date().toISOString().split("T")[1].split(".")[0],
-  //   requestStatus: "Pending",
-  // };
+  const updateRequestStatus = async () => {
+    const updateStatus = "Delivered";
+    const updatedData = {
+      updateStatus,
+    };
+    try {
+      await fetch(
+        `${import.meta.env.VITE_BACKEND_API}/api/v1/request/update/${
+          requestData?._id
+        }`,
+        {
+          method: "PUT",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(updatedData || { updateStatus: "Delivered" }),
+        }
+      );
+    } catch {
+      ("");
+    }
+  };
 
-  // await fetch(`${import.meta.env.VITE_BACKEND_API}/api/v1/food/request/add`, {
-  //   method: "POST",
-  //   headers: {
-  //     "content-type": "application/json",
-  //   },
-  //   body: JSON.stringify(requestFoodData),
-  // }).then(() => {
-  //   toast.success("Request Added", {
-  //     position: "top-center",
-  //     autoClose: 2000,
-  //     hideProgressBar: true,
-  //     closeOnClick: true,
-  //     pauseOnHover: false,
-  //     draggable: true,
-  //     progress: undefined,
-  //     theme: "light",
-  //   });
-  // });
-  //   };
+  //Handle Accept
+  const handleAccept = async () => {
+    try {
+      updateFoodStatus()
+      updateRequestStatus()
+      console.log("Accsepted");
+    } catch {
+      ("");
+    }
+  };
 
   return (
     <div className="my-10">
@@ -152,9 +174,9 @@ const ManageSingleRequest = () => {
 
           {/* Request Details */}
           <div className="min-w-[250px] p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <h5 className="mb-5  border-b border-gray-200 text-xl text-center  font-medium text-gray-900 dark:text-white">
-                Requester
-              </h5>
+            <h5 className="mb-5  border-b border-gray-200 text-xl text-center  font-medium text-gray-900 dark:text-white">
+              Requester
+            </h5>
             <div className="flex flex-col items-center pb-10">
               <img
                 className="w-24 h-24 mb-3 rounded-full shadow-lg"
@@ -165,15 +187,77 @@ const ManageSingleRequest = () => {
                 {requestData?.requesterName}
               </h5>
               <span className="text-sm text-gray-500 dark:text-gray-400">
-               {requestData?.requesterEmail}
+                {requestData?.requesterEmail}
               </span>
               <div className="flex mt-4 space-x-3 md:mt-6">
-                <a
-                  href="#"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                 Accept Request
-                </a>
+                <td className="px-6 py-4">
+                  {/* <!-- Modal toggle --> */}
+                  <button
+                    onClick={() => {
+                      setOpenModal(true);
+                    }}
+                    type="button"
+                    data-modal-target="editUserModal"
+                    data-modal-show="editUserModal"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+                  >
+                    <svg
+                      aria-hidden="true"
+                      role="status"
+                      className="inline w-4 h-4 mr-3 text-white animate-spin"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="#E5E7EB"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                    Loading...
+                  </button>
+
+                  <Modal
+                    show={openModal}
+                    size="md"
+                    onClose={() => setOpenModal(false)}
+                    popup
+                  >
+                    <Modal.Header />
+                    <Modal.Body>
+                      <div className="text-center">
+                        <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                          Are you sure you want to accept this request?
+                        </h3>
+                        <div className="flex justify-center gap-4">
+                          <Button
+                            color="failure"
+                            onClick={() => {
+                              handleAccept(
+                                requestData?.foodId,
+                                requestData?._id
+                              );
+                              setOpenModal(false);
+                            }}
+                          >
+                            {"Yes, I'm sure"}
+                          </Button>
+                          <Button
+                            color="gray"
+                            onClick={() => setOpenModal(false)}
+                          >
+                            No, cancel
+                          </Button>
+                        </div>
+                      </div>
+                    </Modal.Body>
+                  </Modal>
+                </td>
               </div>
             </div>
           </div>
