930c8131b0ebdbaf73be1b7666fdbb94e573f2ea
Md Rashed Ali <tools.rashed@gmail.com>
2023-11-07 23:15:29 +0600

    feature section responsive

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -5,7 +5,7 @@ import Footer from "../Components/Footer/Footer";
 const MainLayout = () => {
   return (
     <div className="dark:bg-gray-900">
-      <div className=" max-w-[1280px] mx-auto lg:px-10">
+      <div className=" max-w-[1280px] mx-auto px-5 lg:px-10">
         <Nav></Nav>
         <Outlet></Outlet>
         <Footer></Footer>
--- src/components/FeatureFoodSection/FeatureFood.jsx
+++ src/components/FeatureFoodSection/FeatureFood.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import { fetchFoodData } from "../../Hooks/fetchData";
 import SectionTitle from "../SectionTitle/SectionTitle";
+import { GrMapLocation } from "react-icons/gr";
+import { CiCalendarDate } from "react-icons/ci";
+import { HiOutlineArrowRight} from 'react-icons/hi';
 import { Link } from "react-router-dom";
+import { Button } from "flowbite-react";
 const FeatureFood = () => {
   const [productData, setProductData] = useState([]);
   useEffect(() => {
@@ -13,76 +17,102 @@ const FeatureFood = () => {
   return (
     <div className="my-[8%]">
       <SectionTitle
-        subtitle="TOP NEW ON THIS WEEK"
-        title="NEW ARRIVALS"
+        subtitle="MORE QUANTITY MORE HAPPINESS"
+        title="FEATURE FOODS"
       ></SectionTitle>
       <div className="grid lg:grid-cols-3 gap-5 ">
         {productData.map((food) => (
           <div key={food._id}>
-           
+            <div className="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+              <div className="w-full">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {food?.foodName}
+                </h5>
 
-              
-      <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <div>
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {food?.foodName}
-                </h5>
-              </a>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology
-              </p>
-            </div>
-        <div className="flex items-center flex-col justify-between gap-5 mb-4">
-          <div className="">
-            <img  src={food?.foodImage} alt="" />
-          </div>
-          <div>
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                 Expire Date
-                </h5>
-              </a>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Pickup Location
-              </p>
-            </div>
-        </div>
-        <div className="flow-root">
-          <ul
-            role="list"
-            className="divide-y divide-gray-200 dark:divide-gray-700"
-          >
-            <li className="pt-3 pb-0 sm:pt-4">
-              <div className="flex items-center space-x-4">
-                <div className="flex-shrink-0">
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {food?.additionalNote}
+                </p>
+              </div>
+              <div className="flex items-center flex-col justify-between gap-5 mb-4">
+                <div className="max-h-[200px] overflow-hidden">
                   <img
-                    className="w-8 h-8 rounded-full"
-                    src={food.donarImage}
-                    alt="Thomas image"
+                    className="h-full w-full object-cover"
+                    src={food?.foodImage}
+                    alt=""
                   />
                 </div>
-
-                <div className="flex-1 min-w-0">
-                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                    {food.donarName}
-                  </p>
-                </div>
-                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                <Link to={`/food/${food._id}`}> <button>Details</button></Link> 
-                </div>
               </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+              <div className="flex justify-between">
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex gap-2 items-center">
+                  <CiCalendarDate className="text-xl text-black"></CiCalendarDate>
+                  <span>{food.expireDate}</span>
+                </p>
 
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex gap-2 items-center">
+                  <GrMapLocation className="text-gray-700 dark:text-gray-400"></GrMapLocation>
 
-           
+                  <span>{food?.pickupLocation}</span>
+                </p>
+              </div>
+              <div className="flow-root">
+                <ul
+                  role="list"
+                  className="divide-y divide-gray-200 dark:divide-gray-700"
+                >
+                  <li className="pt-3 pb-0 sm:pt-4">
+                    <div className="flex items-center space-x-4">
+                      <div className="flex-shrink-0">
+                        <img
+                          className="w-8 h-8 rounded-full object-cover"
+                          src={food?.donarImage}
+                          alt={food?.donarName}
+                        />
+                      </div>
+
+                      <div className="flex-1 min-w-0">
+                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                          {food?.donarName}
+                        </p>
+                      </div>
+                      <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        <Link to={`/food/${food._id}`}>
+                          <button
+                            type="button"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                          >
+                            Details
+                            <svg
+                              className="w-3.5 h-3.5 ml-2"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 14 10"
+                            >
+                              <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M1 5h12m0 0L9 1m4 4L9 9"
+                              />
+                            </svg>
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         ))}
       </div>
-          <Link className="mt-5 text-xl text-center" to="/foods">Show All</Link>
+      <Link className="mt-8 text-xl flex w-full justify-center items-center text-center" to="/foods">
+      <Button>
+        View More
+        <HiOutlineArrowRight className="ml-2 h-5 w-5" />
+      </Button>
+      </Link>
     </div>
   );
 };
