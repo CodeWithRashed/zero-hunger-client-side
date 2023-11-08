5f4a3fb7ebc09ac1eea29534fff7aebdec742fd8
Md Rashed Ali <tools.rashed@gmail.com>
2023-11-08 15:46:32 +0600

    single request page updated

--- src/Pages/ManageSingleRequest.jsx
+++ src/Pages/ManageSingleRequest.jsx
@@ -1,28 +1,18 @@
-import { useState } from "react";
-import { BiSolidDonateHeart, BiSad } from "react-icons/bi";
+import { useEffect, useState } from "react";
 import { Button, Modal } from "flowbite-react";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
-import { useContext, useEffect } from "react";
-
 import { toast } from "react-toastify";
-import { GlobalDataContext } from "../ContextApi/DataContext";
 import { useLoaderData } from "react-router-dom";
 import { Delivered, Pending } from "../Components/Status/Status";
+import { BiSad } from "react-icons/bi";
 
 const ManageSingleRequest = () => {
-  const { activeUser } = useContext(GlobalDataContext);
   const [openModal, setOpenModal] = useState(false);
   const [requestedFood, setRequestedFood] = useState(null);
-  const userEmail = activeUser?.email;
+  const [refetchData, setRefetchData] = useState(null);
+  const [loadingStatus, setLoadingStatus] = useState(false);
+  const [acceptButtonStatus, setAcceptButtonStatus] = useState();
   const requestData = useLoaderData();
-
-  const currentDate = new Date();
-
-  const formattedDate = currentDate.toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  });
 
   useEffect(() => {
     if (requestData?.foodId) {
@@ -37,15 +27,21 @@ const ManageSingleRequest = () => {
           setRequestedFood(data);
         });
     }
+    if (requestData?.requestStatus == "Delivered") {
+      setAcceptButtonStatus("disabled");
+    }
   }, [requestData]);
 
   const updateFoodStatus = async () => {
+    setLoadingStatus(true);
     const updatedData = {
       deliveryStatus: "Delivered",
     };
     try {
       const requestStatusResponse = await fetch(
-        `${import.meta.env.VITE_BACKEND_API}/api/v1/user/update/food/${requestData?.foodId}`,
+        `${import.meta.env.VITE_BACKEND_API}/api/v1/user/update/food/${
+          requestData?.foodId
+        }`,
         {
           method: "PATCH",
           headers: {
@@ -56,7 +52,11 @@ const ManageSingleRequest = () => {
       );
       if (requestStatusResponse.ok) {
         await requestStatusResponse.json();
-  
+        setLoadingStatus(false);
+        setRefetchData(Math.random());
+        console.log(refetchData);
+        setAcceptButtonStatus("disabled");
+        console.log(acceptButtonStatus);
         toast.success("Accepted!", {
           position: "top-right",
           autoClose: 2000,
@@ -74,6 +74,7 @@ const ManageSingleRequest = () => {
   };
 
   const updateRequestStatus = async () => {
+    setLoadingStatus(true);
     const updateStatus = "Delivered";
     const updatedData = {
       updateStatus,
@@ -99,28 +100,28 @@ const ManageSingleRequest = () => {
   //Handle Accept
   const handleAccept = async () => {
     try {
-      updateFoodStatus()
-      updateRequestStatus()
-    } catch {
-      ("");
+      updateRequestStatus();
+      updateFoodStatus();
+    } finally {
+      setRefetchData(Math.random());
     }
   };
 
   return (
     <div className="my-10">
       {requestData ? (
-        <div className="flex gap-5 justify-between ">
+        <div className="grid gap-5 lg:grid-cols-3 justify-between ">
           {/* Food Details */}
-          <div className="flex gap-5 w-full p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <div className="w-1/2 h-full">
+          <div className="col-span-2 flex lg:flex-row flex-col items-center gap-5 lg:w-full p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div className="lg:w-1/2 h-[250px] overflow-hidden">
               <img
-                className="object-cover"
+                className="h-[280px] object-cover"
                 src={requestedFood?.foodImage}
                 alt=""
               />
             </div>
 
-            <div className=" w-1/2">
+            <div className="lg:w-1/2 w-full">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 {requestedFood?.foodName}
               </h5>
@@ -159,10 +160,10 @@ const ManageSingleRequest = () => {
                       Request Status
                     </li>
                     <li className="w-full px-4 py-2 border border-gray-200  dark:border-gray-600">
-                      {requestData?.requestStatus == "Pending" ? (
-                        <Pending></Pending>
-                      ) : (
+                      {acceptButtonStatus ? (
                         <Delivered></Delivered>
+                      ) : (
+                        <Pending></Pending>
                       )}
                     </li>
                   </div>
@@ -198,26 +199,34 @@ const ManageSingleRequest = () => {
                     type="button"
                     data-modal-target="editUserModal"
                     data-modal-show="editUserModal"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+                    className={`${
+                      acceptButtonStatus && "cursor-not-allowed"
+                    } text-white px-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  py-2 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center`}
                   >
-                    <svg
-                      aria-hidden="true"
-                      role="status"
-                      className="inline w-4 h-4 mr-3 text-white animate-spin"
-                      viewBox="0 0 100 101"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="#E5E7EB"
-                      />
-                      <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                    Loading...
+                    {loadingStatus && (
+                      <svg
+                        aria-hidden="true"
+                        role="status"
+                        className="inline w-4 h-4 mr-3 text-white animate-spin"
+                        viewBox="0 0 100 101"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                          fill="#E5E7EB"
+                        />
+                        <path
+                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                          fill="currentColor"
+                        />
+                      </svg>
+                    )}
+                    {loadingStatus
+                      ? "Loading..."
+                      : acceptButtonStatus
+                      ? "Request Accepted"
+                      : "Accept Request"}
                   </button>
 
                   <Modal
@@ -263,8 +272,10 @@ const ManageSingleRequest = () => {
         </div>
       ) : (
         <div className="flex min-h-[50vh] justify-center flex-col items-center w-full p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-         <BiSad className="text-4xl font-semibold text-gray-900 dark:text-white"></BiSad>
-          <h1 className=" text-xl md:text-3xl lg:text-4xl font-semibold text-gray-900 dark:text-white">No One Requested For This Food</h1>
+          <BiSad className="text-4xl font-semibold text-gray-900 dark:text-white"></BiSad>
+          <h1 className=" text-xl md:text-3xl lg:text-4xl font-semibold text-gray-900 dark:text-white">
+            No One Requested For This Food
+          </h1>
         </div>
       )}
     </div>
