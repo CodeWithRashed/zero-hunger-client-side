62d86cc638409d85711720a1bb68b6f01aef68ed
Md Rashed Ali <tools.rashed@gmail.com>
2023-11-08 23:11:31 +0600

    dark mode optimize

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import logo from '../../assets/logo.png'
 
 const Footer = () => {
   return (
-    <div>
+    <div className="pb-5">
       
-      <footer className="bg-gray-900 w-full">
+      <footer className="bg-gray-100 px-2 py-2.5 dark:border-gray-700 dark:bg-gray-800 sm:px-4 rounded-xl text-gray-800 dark:text-gray-300 hover:dark:text-gray-200  w-full">
   <div className="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 lg:pt-20 mx-auto">
     {/* <!-- Grid --> */}
     <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
@@ -20,9 +20,9 @@ const Footer = () => {
         <h4 className="font-semibold text-gray-100">Menu</h4>
 
         <div className="mt-3 grid space-y-3">
-          <p><Link className="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/">Home</Link></p>
-          <p><Link className="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/food">Foods</Link></p>
-          <p><Link className="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/login">Login</Link></p>
+          <p><Link className="inline-flex gap-x-2 text-gray-800 dark:text-gray-300 hover:dark:text-gray-200  hover:text-gray-900 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/">Home</Link></p>
+          <p><Link className="inline-flex gap-x-2 text-gray-800 dark:text-gray-300 hover:dark:text-gray-200  hover:text-gray-900 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/food">Foods</Link></p>
+          <p><Link className="inline-flex gap-x-2 text-gray-800 dark:text-gray-300 hover:dark:text-gray-200  hover:text-gray-900 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/login">Login</Link></p>
         </div>
       </div>
       {/* <!-- End Col --> */}
@@ -31,9 +31,9 @@ const Footer = () => {
         <h4 className="font-semibold text-gray-100">Admin</h4>
 
         <div className="mt-3 grid space-y-3">
-          <p><Link className="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/add">Add Food</Link></p>
-          <p><Link className="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/manage">Manage</Link></p>
-          <p><Link className="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/request">Request</Link> </p>
+          <p><Link className="inline-flex gap-x-2 text-gray-800 dark:text-gray-300 hover:dark:text-gray-200  hover:text-gray-900 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/add">Add Food</Link></p>
+          <p><Link className="inline-flex gap-x-2 text-gray-800 dark:text-gray-300 hover:dark:text-gray-200  hover:text-gray-900 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/manage">Manage</Link></p>
+          <p><Link className="inline-flex gap-x-2 text-gray-800 dark:text-gray-300 hover:dark:text-gray-200  hover:text-gray-900 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/request">Request</Link> </p>
           
         </div>
       </div>
@@ -46,7 +46,7 @@ const Footer = () => {
           <div className="mt-4 flex flex-col items-center gap-2 sm:flex-row sm:gap-3 bg-white rounded-lg p-2 dark:bg-gray-800">
             <div className="w-full">
               <label htmlFor="hero-input" className="sr-only">Search</label>
-              <input type="text" id="hero-input" name="hero-input" className="py-3 px-4 block w-full border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-transparent dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Enter your email"/>
+              <input type="text" id="hero-input" name="hero-input" className="py-3 px-4 block w-full border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-transparent dark:text-gray-800  hover:dark:text-gray-200  dark:focus:ring-gray-600" placeholder="Enter your email"/>
             </div>
             <a className="w-full sm:w-auto whitespace-nowrap p-3 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
               Subscribe
@@ -61,7 +61,7 @@ const Footer = () => {
 
     <div className="mt-5  sm:mt-12 grid gap-y-2 sm:gap-y-0 sm:flex sm:justify-between sm:items-center">
       <div className="flex justify-between items-center">
-        <p className="text-sm  text-gray-400">© 2022 Zero Hunger. All rights reserved.</p>
+        <p className="text-sm  text-gray-800 dark:text-gray-300 hover:dark:text-gray-200 ">© 2022 Zero Hunger. All rights reserved.</p>
       </div>
       {/* End Col */}
 
